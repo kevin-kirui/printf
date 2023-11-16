b8e91dea7b59aaedd7f647c5197a5dabae0141a4
kevin kirui <kipkoechkevin42@gmail.com>
2023-11-15 22:44:23 -0800

    kevin

--- main.c
+++ main.c
@@ -32,12 +32,10 @@ int main(void)
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	printf("Unknown:[%s]\n");
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	printf("Unknown:[%r]\n");
-	return (0);
-}
+	perintf("Len:[%d]\n", len2);
--- print_get_flags.c
+++ print_get_flags.c
@@ -12,25 +12,30 @@ int main(void)
 }
 void handle_flags(va_list val, int flags, char specifier)
 {
+	char buffer[100];
 	int pos = 0;
 
-	if (flags & 1)
-	{
-		buffer[pos++] = '+';
-	}
-	if (flags & 2)
-	{
-		buffer[pos++] = ' ';
-	}
-	if (flags & 4)
-	{
-		buffer[pos++] = '#';
-	}
-	buffer[pos++] = specifier;
-	buffer[pos] = '\0';
+	buffer[pos++] = '+';
 
 	for (int i = 0; buffer[i] != '\0'; i++)
 	{
-		putchar(buffer[i]);
+		if (flags & 1)
+		{
+			buffer[pos++] = '+';
+		}
+		if (flags & 2)
+		{
+			buffer[pos++] = ' ';
+		}
+		if (flags & 4)
+		{
+			buffer[pos++] = '#';
+		}
+		buffer[pos++] = specifier;
+		buffer[pos] = '\0';
+
+		for (int i = 0; buffer[i] != '\0'; i++)
+		{
+			putchar(buffer[i]);
+		}
 	}
-}
