e4a4f404a5f09874461dae7eb0c2bcdb241a4ce6
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-17 19:27:28 +0300

    Polycarp

--- main.h
+++ main.h
@@ -5,12 +5,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
+
+/**
+ * struct format - converter for printf
+ * @ph: type char pointer of specifier
+ * @function: function for conversion specifier
+ */
 
 typedef struct format
 {
-        char *id;
-        int (*f)();
-}match;
+        char *ph;
+        int (*function)();
+} convert;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int putss(char *c);
