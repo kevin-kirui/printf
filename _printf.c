04c04698f6ac50ce5e543ac690082ea7395ed928
GitongaPolycarp <memepolycarp@gmail.com>
2023-11-20 19:56:50 +0300

    Polycarp

--- _printf.c
+++ _printf.c
@@ -1,74 +1,43 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: Format string.
- * Return: Printed characters.
+ * _printf - function
+ * @format: format.
+ * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	unsigned int i, count = 0;
+	va_list args;
 
-	if (!format)
-	{
-		return (-1);
-	}
+	va_start(args, format);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-
-			if (buff_ind == BUFF_SIZE)
-			{
-				print_buffer(buffer, &buff_ind);
-			}
-
-			printed_chars++;
+			putchar(format[i]);
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-
-			printed = handle_print(format, &i, list, buffer,				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-
-			printed_chars += printed;
+			switch (format[++i])
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += putss(va_arg(args, char *)) - 1;
+					break;
+				case '%':
+					putchar('%');
+					break;
+				default:
+					break;
+			}
 		}
+		count++;
 	}
 
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints contents of buffer if it exists.
- * @buffer: Array of characters.
- * @buff_ind: Index at which to add the next character, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-	}
-	*buff_ind = 0;
+	va_end(args);
+	return (count);
 }
