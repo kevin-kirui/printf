f7a99c41e1d1349e77c60cf12d82538da2054a36
kevin kirui <kipkoechkevin42@gmail.com>
2023-11-24 01:22:45 -0800

    -a

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h" 
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_printf("Hello, %s! Today is %c\n", "world", 'A');
+    return 0;
+}
+
