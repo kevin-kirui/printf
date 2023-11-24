1bab479d00d188b1909f8b2ee0c208f6a62180a6
kevin kirui <kipkoechkevin42@gmail.com>
2023-11-24 00:42:35 -0800

    -a

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, count = 0;
+	unsigned int i, count = 0, s_count = 0;
 	va_list args;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
@@ -30,22 +30,16 @@ int _printf(const char *format, ...)
 		}
 		else if (format[i + 1] == 's')
 		{
-			count += putss(va_arg(args, const char *));
+			count += putss(va_arg(args, char*));
 			i++;
+			count += (s_count - 1);
 		}
 		else if (format[i + 1] == '%')
 		{
 			my_putchar('%');
-			count++;
 		}
-		else
-		{
-			my_putchar('%');
-			my_putchar(format[i]);
-			count += 2;
-		}
+		count += 1;
 	}
-
 	va_end(args);
 	return (count);
 }
